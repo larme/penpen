c89b2587edc95877880f8e068aecf914b37569ad
Zhao Shenyang <dev@zsy.im>
2021-01-04 06:45:06 +0800

    modify osc-listen function

--- osc.lisp
+++ osc.lisp
@@ -3,7 +3,7 @@
 (in-package #:penpen)
 
 
-(defun osc-listen (port &optional (buff_size 2048))
+(defun osc-listen (msg-handler port &optional (buff_size 2048))
   "listen to a port"
   (let ((buffer (make-array buff_size :element-type '(unsigned-byte 8))))
     (usocket:with-connected-socket
@@ -14,6 +14,7 @@
       (format t "osc listener ~a start at port ~a!~%" s port)
       (loop
 	(usocket:socket-receive s buffer buff_size)
-	(let* ((content (osc:decode-bundle buffer)))
-	  (format t "get ~a~%" content)
-	  (format t "type is ~a~%" (type-of content)))))))
+	(let* ((msg (osc:decode-bundle buffer))
+	       (terminatep (funcall msg-handler msg)))
+	  (when terminatep
+	    (return)))))))
